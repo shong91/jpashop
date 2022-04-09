c7786dc60e9dc3d687d7623db7a43c6d32b49a75
shong91 <hhhongso@gmail.com>
2022-04-09 17:21:38 +0900

    Add Entity class (2)

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,10 +6,23 @@ import lombok.Setter;
 
 @Embeddable
 @Getter
-@Setter
 public class Address {
 
   private String city;
   private String street;
   private String zipcode;
+
+  protected Address() {
+    // JPA spec 상 @Entity, @Embeddable 타입은 기본 생성자를 public or protected 로 설정하여야 한다.
+    // 안전성을 위해 public 보다는 protected 를 사용하는 편
+    // why? JPA 구현 라이브러리가 객체를 생성할 대 리플랙션 같은 기술을 사용할 수 있도록 지원해야 하기 때문
+  }
+
+  // 값 타임은 변경 불가하게 설계한다.
+  // why? @Setter 를 열어두면 애플리케이션이 커질수록 어디서 무엇을 변경하였는지 추적하기 어려워짐
+  public Address(String city, String street, String zipcode) {
+    this.city = city;
+    this.street = street;
+    this.zipcode = zipcode;
+  }
 }
--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,7 +28,20 @@ public class Category {
   private String name;
 
   @ManyToMany
-  @JoinTable(name = "category_item", joinColumns = @JoinColumn(name = "category_id"), inverseJoinColumns = @JoinColumn(name = "item_id"))
-  // 실무에서 ManyToMany 는 지양 !!
+  @JoinTable(name = "category_item",
+      joinColumns = @JoinColumn(name = "category_id"),
+      inverseJoinColumns = @JoinColumn(name = "item_id"))
   private List<Item> items = new ArrayList<>();
+  // @ManyToMany 는 실무에 적용하기에는 한계가 있음 !!
+  // why? 중간 테이블에 컬럼을 추가할 수 없고(joinColumn 만 가능하며, 공통컬럼 등 기본 컬럼 세팅 불가), 세밀하게 쿼링하기 어려움.
+  // => 실무에서는 다대다 매핑(@ManyToMany) 을 그대로 사용하지 말고,
+  // 중간 엔티티를 만들고 일대다-다대일 매핑(@ManyToOne - @OneToMany)으로 풀어내어 사용하자.
+
+  @ManyToOne
+  @JoinColumn(name = "parent_id")
+  private Category parent;
+
+  @OneToMany(mappedBy = "parent")
+  private List<Category> child = new ArrayList<>();
+
 }
