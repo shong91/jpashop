1056cba54b2d23a9262acbef43975a19ac3d33f2
shong91 <hhhongso@gmail.com>
2022-04-19 22:50:02 +0900

    Add Item/ add Item entity business logic

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -13,6 +13,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,7 +21,6 @@ import lombok.Setter;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
 public abstract class Item {
 
   @Id
@@ -37,4 +37,29 @@ public abstract class Item {
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
 
+  // 객체지향적 관점으로 보았을 때, 데이터를 가진 쪽에서 비즈니스 로직을 처리하는 것이 가장 응집력이 있다.
+  // DDD 에서는 엔티티가 데이터를 가지기 때문에, 엔티티에서 해당 비즈니스 로직을 구현함.
+  // stockQuantity 를 변경할 때, setter 메서드를 사용하는 것이 아니라 해당 엔티티 안에서 처리하는 것!
+
+  /**
+   * stock 증가
+   *
+   * @param quantity
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * stock 감소
+   *
+   * @param quantity
+   */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 }
