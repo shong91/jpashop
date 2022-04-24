3788d50d45c4458b99eede01f670c3fe973c25d3
shong91 <hhhongso@gmail.com>
2022-04-24 17:55:06 +0900

    Add Order/ 주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.repository;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,21 @@ public class OrderRepository {
     return em.find(Order.class, id);
   }
 
-//  public List<Order> findAll(OrderSearch orderSearch) {}
+  public List<Order> findAll(OrderSearch orderSearch) {
+    // 정적쿼리
+    return em.createQuery("select o from Order o join o.member m "
+            + "where o.status = :status"
+            + "and m.name like :name", Order.class)
+        .setParameter("status", orderSearch.getOrderStatus())
+        .setParameter("name", orderSearch.getMemberName())
+        .setFirstResult(100) // 페이징 시 변수 설정
+        .setMaxResults(1000)
+        .getResultList();
+
+    // 동적쿼리 만들기
+    // 1. 직접 jpql string 생성
+    // 2. JPA criteria 를 통한 jpql 생성
+    // 3. Querydsl (권장)
+  }
 
 }
